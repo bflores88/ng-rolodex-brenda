eb7af2b51f8fa0c1b223bebb263f82fdb39b7216
kmietotwy <kmietotwy@gmail.com>
2019-05-21 09:38:17 -1000

    POST to logout working on server via postman

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -2,12 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../database/models/User.js');
-const Contact = require('../database/models/Contact.js');
 
-router.route('/index')
-  .get((req, res) => {
-
+router.route('/')
+  .post((req, res) => {
+    req.logout();
+    return res.send('{ success: logged out }')
   })
 
 module.exports = router;
