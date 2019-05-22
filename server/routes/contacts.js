315d301af6502df02e6e00e0b68556ae34744ce3
kmietotwy <kmietotwy@gmail.com>
2019-05-21 15:23:50 -1000

    GET search term by user working on server

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -5,13 +5,44 @@ const router = express.Router();
 const User = require('../database/models/User.js');
 const Contact = require('../database/models/Contact.js');
 
-router.route('/').get((req, res) => {
-  let user = req.query.user;
+router
+  .route('/')
+  .get((req, res) => {
+    let user = req.query.user;
 
-  Contact.where({ created_by: user })
-    .fetchAll({ withRelated: ['users'] })
+    Contact.where({ created_by: user })
+      .fetchAll({ withRelated: ['users'] })
+      .then((result) => {
+        return res.json(result.toJSON());
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  })
+  .post((req, res) => {
+    new Contact({
+      name: req.body.name,
+      created_by: req.body.created_by,
+      address: req.body.address,
+    })
+      .save()
+      .then((result) => {
+        return res.json(result.toJSON());
+      })
+      .catch((err) => {
+        console.log('error', err);
+        return res.send(err);
+      });
+  });
+
+router.route('/search/:term').get((req, res) => {
+  let user = req.query.user;
+  Contact.query(function(qb) {
+    qb.where('created_by', '=', user).andWhere('name', 'LIKE', `%${req.params.term}%`);
+  })
+    .fetchAll({ withRelated: 'users' })
     .then((result) => {
-      return res.send(result);
+      return res.json(result.toJSON());
     })
     .catch((err) => {
       console.log('error', err);
