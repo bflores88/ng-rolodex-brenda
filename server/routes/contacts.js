43b7ea6bab7fcabf8ef1f6a534db5e0e7a96c01c
kmietotwy <kmietotwy@gmail.com>
2019-05-21 15:43:13 -1000

    PUT to contacts by id working on server side

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -42,6 +42,28 @@ router
       });
   });
 
+router.route('/:id')
+  .get((req, res) => {
+    new Contact({ id: req.params.id })
+      .fetch({ withRelated: ['users'] })
+      .then((result) => {
+        return res.json(result.toJSON());
+      })
+      .catch((err) => {
+        console.log('error', err);
+    })
+  })
+  .put((req, res) => {
+    new Contact({ id: req.params.id })
+      .save(checkBodyUpdates(req.body))
+      .then((result) => {
+        return res.json(result.toJSON());
+      })
+      .catch((err) => {
+        console.log('error', err);
+    })
+  })
+
 router.route('/search/:term').get((req, res) => {
   let user = req.query.user;
   Contact.query(function(qb) {
@@ -55,5 +77,52 @@ router.route('/search/:term').get((req, res) => {
       console.log('error', err);
     });
 });
+
+function checkBodyUpdates(requestBody) {
+  let updatedContact = {};
+
+  if (requestBody.name) {
+    updatedContact.name = requestBody.name;
+  }
+
+  if (requestBody.address) {
+    updatedContact.address = requestBody.address;
+  }
+
+  if (requestBody.mobile) {
+    updatedContact.mobile = requestBody.mobile;
+  }
+
+  if (requestBody.work) {
+    updatedContact.work = requestBody.work;
+  }
+
+  if (requestBody.home) {
+    updatedContact.home = requestBody.home;
+  }
+
+  if (requestBody.email) {
+    updatedContact.email = requestBody.email;
+  }
+
+  if (requestBody.twitter) {
+    updatedContact.twitter = requestBody.twitter;
+  }
+
+  if (requestBody.instagram) {
+    updatedContact.instagram = requestBody.instagram;
+  }
+
+  if (requestBody.github) {
+    updatedContact.github = requestBody.github;
+  }
+
+  if (requestBody.created_by) {
+    updatedContact.created_by = requestBody.created_by;
+  }
+
+  return updatedContact;
+
+}
 
 module.exports = router;
